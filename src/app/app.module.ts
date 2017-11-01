78a6bb60cde8d611ac6f6f665ab5e2913025b8d9
Alexander <alex1990pettersen@gmail.com>
2017-11-01 12:44:12 +0100

    merge fra sindre

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,6 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { BodyComponent } from './body/body.component';
 import { FooterComponent } from './footer/footer.component';
-<<<<<<< HEAD
-import { DokumenterComponent } from './dokumenter/dokumenter.component';
-import { DokumentComponent } from './dokumenter/dokument/dokument.component';
-=======
->>>>>>> d9951d566f9a35832ac93bcb8cf04247babefac5
 //import { LogoComponent } from 'body/logo/logo.component';
 
 @NgModule({
@@ -18,11 +13,6 @@ import { DokumentComponent } from './dokumenter/dokument/dokument.component';
     HeaderComponent,
     BodyComponent,
     FooterComponent,
-<<<<<<< HEAD
-    DokumenterComponent,
-    DokumentComponent,
-=======
->>>>>>> d9951d566f9a35832ac93bcb8cf04247babefac5
     //LogoComponent
   ],
   imports: [
