f83871ad9c9964f8a092bdfecff1ffbe38c0a818
Erik <erik_hellerslien@hotmail.com>
2017-11-01 12:02:34 +0100

    asd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,22 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { BodyComponent } from './body/body.component';
-<<<<<<< HEAD
-=======
-import { FooterComponent } from './footer/footer.component';
->>>>>>> d9951d566f9a35832ac93bcb8cf04247babefac5
 //import { LogoComponent } from 'body/logo/logo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-<<<<<<< HEAD
     BodyComponent
-=======
-    BodyComponent,
-    FooterComponent,
->>>>>>> d9951d566f9a35832ac93bcb8cf04247babefac5
     //LogoComponent
   ],
   imports: [
