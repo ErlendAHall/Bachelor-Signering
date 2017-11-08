161344fd0903c8a5acb5f1ec7eb594ac81058340
Sindre Østby <sindreoestby1@gmail.com>
2017-11-08 11:52:43 +0100

    jobber

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { BodyComponent } from './body/body.component';
-//import { LogoComponent } from 'body/logo/logo.component';
+import { DokumenterComponent } from './dokumenter/dokumenter.component';
+import { DokumentComponent } from './dokumenter/dokument/dokument.component';
+import { MenyComponent } from './meny/meny.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    BodyComponent
-    //LogoComponent
+    BodyComponent,
+    DokumenterComponent,
+    DokumentComponent,
+    MenyComponent
   ],
   imports: [
     BrowserModule
