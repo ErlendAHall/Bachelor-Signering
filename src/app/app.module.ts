0ce171c3876f4f45736e5ca7b8dd2607a20322e0
Alexander Pettersen <Alex1990pettersen@gmail.com>
2017-11-08 13:53:02 +0100

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,30 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { BodyComponent } from './body/body.component';
+import { DokumenterComponent } from './dokumenter/dokumenter.component';
+import { DokumentComponent } from './dokumenter/dokument/dokument.component';
 import { UtloggetComponent } from './utlogget/utlogget.component';
 import { InnloggetComponent } from './innlogget/innlogget.component';
-//import { LogoComponent } from 'body/logo/logo.component';
+import{Routes, RouterModule} from '@angular/router';
+
+const appRoutes: Routes = [
+  {path:'', component: UtloggetComponent},
+  {path:'innlogget', component: InnloggetComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     BodyComponent,
+    DokumenterComponent,
+    DokumentComponent,
     UtloggetComponent,
     InnloggetComponent
-    //LogoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
