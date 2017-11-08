95187b6aebf37c0a6ffaccff4436fa405b3e48cb
Erik <erik_hellerslien@hotmail.com>
2017-11-08 11:54:30 +0100

    sd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { BodyComponent } from './body/body.component';
+import { DokumenterComponent } from './dokumenter/dokumenter.component';
+import { DokumentComponent } from './dokumenter/dokument/dokument.component';
 //import { LogoComponent } from 'body/logo/logo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    BodyComponent
+    BodyComponent,
+    DokumenterComponent,
+    DokumentComponent
     //LogoComponent
   ],
   imports: [
