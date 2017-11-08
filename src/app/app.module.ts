ac7d96dd4c1fef1bbd8bdc2a6f22b5e3945089a0
Alexander Pettersen <Alex1990pettersen@gmail.com>
2017-11-08 13:46:45 +0100

    nye komponenter for routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { BodyComponent } from './body/body.component';
+import { UtloggetComponent } from './utlogget/utlogget.component';
+import { InnloggetComponent } from './innlogget/innlogget.component';
 //import { LogoComponent } from 'body/logo/logo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    BodyComponent
+    BodyComponent,
+    UtloggetComponent,
+    InnloggetComponent
     //LogoComponent
   ],
   imports: [
---
+++ src/app/innlogget/innlogget.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-innlogget',
+  templateUrl: './innlogget.component.html',
+  styleUrls: ['./innlogget.component.css'],
+  
+})
+export class InnloggetComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
