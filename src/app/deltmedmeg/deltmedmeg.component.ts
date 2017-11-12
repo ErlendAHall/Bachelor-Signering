fdb4052e3d1ca0f8abbe4a505942fc62e08ffaad
Erik <erik_hellerslien@hotmail.com>
2017-11-12 20:21:33 +0100

    Lagt til noen komponenter og stuff, linket opp sider

---
+++ src/app/deltmedmeg/deltmedmeg.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { ViewEncapsulation } from '@angular/core';
+
+
+@Component({
+  selector: 'app-deltmedmeg',
+  templateUrl: './deltmedmeg.component.html',
+  styleUrls: ['./deltmedmeg.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class DeltmedmegComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/nylige/nylige.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { ViewEncapsulation } from '@angular/core';
+
+
+@Component({
+  selector: 'app-nylige',
+  templateUrl: './nylige.component.html',
+  styleUrls: ['./nylige.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class NyligeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
