7797230aa7a5792375f7b36ef8ef62435deacf37
Erik <erik_hellerslien@hotmail.com>
2017-11-12 17:08:48 +0100

    Småfixces

--- src/app/utlogget/utlogget.component.ts
+++ src/app/utlogget/utlogget.component.ts
@@ -4,7 +4,6 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-utlogget',
   templateUrl: './utlogget.component.html',
   styleUrls: ['./utlogget.component.css'],
-  
 })
 export class UtloggetComponent implements OnInit {
 
