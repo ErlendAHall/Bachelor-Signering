74e39907a4446644345defe11448aa8a9ad8f96d
Alexander Pettersen <Alex1990pettersen@gmail.com>
2017-11-13 09:55:25 +0100

    merge

--- src/app/utlogget/utlogget.component.ts
+++ src/app/utlogget/utlogget.component.ts
@@ -3,12 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-utlogget',
   templateUrl: './utlogget.component.html',
-<<<<<<< HEAD
-  styleUrls: ['./utlogget.component.css'],
-  encapsulation: ViewEncapsulation.None
-=======
   styleUrls: ['./utlogget.component.scss'],
->>>>>>> 5774bf19f90eda51de2cb2b340fa741bf95b9db5
 })
 export class UtloggetComponent implements OnInit {
 
