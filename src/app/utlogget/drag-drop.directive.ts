678d9b5d9f7bd2fcf289ec21e846afa870170f1a
JesusFrelseren <ErlendAHall@gmail.com>
2017-11-17 14:45:47 +0100

    drag and drop, sjekker på pdf-filer

--- src/app/utlogget/drag-drop.directive.ts
+++ src/app/utlogget/drag-drop.directive.ts
@@ -1,4 +1,4 @@
-import {Directive, EventEmitter, HostListener, Output, HostBinding} from '@angular/core';
+import {Directive, EventEmitter, HostListener, Output, HostBinding, Input} from '@angular/core';
 
 // Directive deklarasjon og HTML selektor
 @Directive({
@@ -8,8 +8,9 @@ import {Directive, EventEmitter, HostListener, Output, HostBinding} from '@angul
 // Directive kontrollklasse
 export class DragDropDirective {
 
+    private files: Array<File> = [];
     @HostBinding('style.background') private background;
-
+    @Input() private allowedExtensions: Array<string> = [];
       constructor() { }
 
       @HostListener('dragover', ['$event']) public onDragOver(evt) {
@@ -25,17 +26,35 @@ export class DragDropDirective {
       @HostListener('drop', ['$event']) public onDrop(evt) {
         evt.preventDefault();
         evt.stopPropagation();
-        this.background = 'rgba(2,48,84,0.7)';
-        const files = evt.dataTransfer.files;
-        if (files.length > 0) {
-            console.log(files);
+        this.background = '';
+        this.files = evt.dataTransfer.files;
+        const valid_docs: Array<File> = [];
+        console.log(this.files);
+        if (this.files.length > 0) {
+            for (let file of this.files) {
+                // Splitter opp filnavn til navn og extension
+                console.log(file.name.toLowerCase());
+                const extArray = file.name.split('.');
+                const ext = extArray[1].toLowerCase();
+
+                // Hvis extension === .pdf (eller andre), push file til valid_docs
+                if (this.allowedExtensions.lastIndexOf(ext) !== -1) {
+                    valid_docs.push(file);
+                }
+            }
+        }
+
+        if (valid_docs.length === 0) {
+            alert('Feil filformat');
+        } else if (valid_docs.length < this.files.length) {
+            alert('Noen av dokumentene var ikke i gyldig format');
         }
       }
 
 }
 
-// todo: filtrer filer
 export class DragDropComponent {
+
     private fileList: any = [];
     constructor() { }
     onFilesChange(fileList: FileList) {
